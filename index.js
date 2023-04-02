07398f71f45d90003e7cad0974ff82f0165387a3
AyimWilliam <odhisayim@gmail.com>
2023-04-02 22:58:45 +0300

    Completely Updated

--- index.js
+++ index.js
@@ -1,75 +1,83 @@
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
+  const moviePoster = document.getElementById("moviePoster");
+  const movieTitle = document.getElementById("movieTitle");
+  const movieRuntime = document.getElementById("movieRuntime");
+  const movieShowtime = document.getElementById("movieShowtime");
+  const movieAvailableTickets = document.getElementById(
+    "movieAvailableTickets"
+  );
+  const buyTicketBtn = document.getElementById("buyTicketbtn");
+  const filmList = document.getElementById("films");
 
-    const moviePoster = document.getElementById("moviePoster");
-    const movieTitle = document.getElementById("movieTitle");
-    const movieRuntime = document.getElementById("movieRuntime");
-    const movieShowtime = document.getElementById("movieShowtime");
-    const movieAvailableTickets = document.getElementById("movieAvailableTickets");
-    const buyTicketBtn = document.getElementById("buyTicketbtn");
-    const filmList = document.getElementById("films");
-    
-    //We fetch the movie data from API and populates first movie details on page load
+  //We fetch the movie data from API and populates first movie details on page load
 
-    fetch("http://localhost:3000/films")
-    .then(response => response.json())
-    .then(movie => {
-        moviePoster.src = movie.poster;
-        movieTitle.innerText = movie.title;
-        movieRuntime.innerText = `Runtime: ${movie.runtime} minutes`;
-        movieShowtime.innerText = `Showtime: ${movie.showtime} minutes`;
-        const availableTickets = movie.capacity - movie.tickets_sold;
-        movieAvailableTickets.innerText = `Available Tickets: ${availableTickets}`;
-
+  fetch("http://localhost:3000/films")
+    .then((response) => response.json())
+    .then((movie) => {
+      moviePoster.src = movie.poster;
+      movieTitle.innerText = movie.title;
+      movieRuntime.innerText = `Runtime: ${movie.runtime} minutes`;
+      movieShowtime.innerText = `Showtime: ${movie.showtime} minutes`;
+      const availableTickets = movie.capacity - movie.tickets_sold;
+      movieAvailableTickets.innerText = `Available Tickets: ${availableTickets}`;
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 
-    //Fetch movie data from API or jso and populates film list on page load
-    fetch('http://localhost:3000/films')
-    .then(response => response.json())
-    .then(movies => {
-     // clear placeholder li element from the fllm list
-     filmList.innerHTML = "";
-     //Loop through make movies and create li elemwnt for each one 
-     movies.forEach(movie => {
+  //Fetch movie data from API or jso and populates film list on page load
+  fetch("http://localhost:3000/films")
+    .then((response) => response.json())
+    .then((movies) => {
+      // clear placeholder li element from the fllm list
+      filmList.innerHTML = "";
+      //Loop through make movies and create li elemwnt for each one
+      movies.forEach((movie) => {
         const filmItem = document.createElement("li");
         filmItem.classList.add("filmItem");
         filmItem.innerText = mpovie.title;
-    
-    // Add eben listener to li elemt to populate movie details and when clicked
-        filmItem.addEventListener('click',() =>{
-            moviePoster.src = movie.poster;
-            movieTitle.innerText = movie.title;
-            movieRuntime.innerText = `Runtime: ${movie.runtime} minutes`;
-          movieShowtime.innerText = `Showtime: ${movie.showtime}`;
-          const availableTickets = movie.capacity - movie.tickets_sold;
-          movieAvailableTickets.innerText = `Available Tickets: ${availableTickets}`;
 
-          // Disable buy ticket button if no available tickets
-          buyTicketBtn.disabled = availableTickets <= 0;
+        // Add eben listener to li elemt to populate movie details and when clicked
+        filmItem.addEventListener("click", () => {
+          fetch(`http://localhost:3000/films/${movie.id}`)
+            .then((response) => response.json())
+            .then((newMovie) => {
+              moviePoster.src = newMovie.poster;
+              movieTitle.innerText = newMovie.title;
+              movieRuntime.innerText = `Runtime: ${newMovie.runtime} minutes`;
+              movieShowtime.innerText = `Showtime: ${newMovie.showtime}`;
+              const availableTickets =
+                newMovie.capacity - newMovie.tickets_sold;
+              movieAvailableTickets.innerText = `Available Tickets: ${availableTickets}`;
+
+              // Disable buy ticket button if no available tickets
+              buyTicketBtn.disabled = availableTickets <= 0;
+            })
+            .catch((error) => console.error(error));
         });
 
-        filmList.appendChild(filmItem);
-
-     });
-    })
-    .catch(error => console.log(error));
-
-    //Add event listener to buy ticket button
-    buyTicketBtn.addEventListener("click", () => {
-    // get current available tickes from moive details
-    const currentAvailableTickets = parseInt(movieAvailableTickets.innerText.split(" ")[2]);
-    if(currentAvailableTickets > 0){
-        //update available tickets on frontend and API
-        const updatedAvailableTickets = currentAvailableTickets - 1;
-        movieAvailableTickets.innerText = `Available Tickets: ${updatedAvailableTickets}`;
-        fetch('http://localhost:3000/films',{
-            method: "PATCH",
-            headers: {"Content-Type": "application/json"
-        },
-        body: JSON.stringify({tickets_sold: movie.capacity - updatedAvailableTickets})
-        })
-        .then(response => response.json())
-        .catch(error => console.error(error));
-    }
+        //Add event listener to buy ticket button
+        buyTicketBtn.addEventListener("click", () => {
+            // get current available tickets from movie details
+            const currentAvailableTickets = parseInt(movieAvailableTickets.innerText.split(" ")[2]);
+            if (currentAvailableTickets > 0) {
+                // update available tickets on frontend and API
+                const updatedAvailableTickets = currentAvailableTickets - 1;
+                movieAvailableTickets.innerText = `Available Tickets: ${updatedAvailableTickets}`;
+                fetch('http://localhost:3000/films', {
+                    method: "PATCH",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ tickets_sold: movie.capacity - updatedAvailableTickets })
+                })
+                    .then(response => response.json())
+                    .catch(error => console.error(error));
+            } else {
+                // indicate that movie is sold out
+                buyTicketBtn.innerText = "Sold Out";
+                buyTicketBtn.disabled = true;
+        
+                const filmItem = document.querySelector(`li.filmItem[data-movie-id="${movie.id}"]`);
+                filmItem.classList.add("sold-out");
+            }
+        });                                      
+      });
     });
 });
