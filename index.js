9fef77143f507204cff1fc0fd11f8052fe8e02bb
AyimWilliam <odhisayim@gmail.com>
2023-04-02 20:20:13 +0300

    declared variables in js file

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",() => {
+
+    const moviePoster = document.getElementById("moviePoster");
+    const movieTitle = document.getElementById("movieTitle");
+    const movieRuntime = document.getElementById("movieRuntime");
+    const movieShowtime = document.getElementById("movieShowtime");
+    const movieAvailableTickets = document.getElementById("movieAvailableTickets");
+    const buyTicketsBtn = document.getElementById("buyTicketbtn");
+    const filmList = document.getElementById("films");
+    
+})
